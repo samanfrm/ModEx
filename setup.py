e5c898b2fbbb290111b743cc783b93132f382f18
Saman Farahmand <samanfrm@samans-mbp.ssw.umb.edu>
2019-06-10 12:37:16 -0400

    setup.py update3

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setuptools.setup(
         "Operating System :: OS Independent"
     ],
     install_requires=['numpy', 'pandas', 'biopython', 'nltk','networkx','jellyfish',
-                      'becas','requests','string','json','re'],
+                      'becas','requests','string','json'],
     zip_safe=False,
 )
