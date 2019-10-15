b440fb69870960e2c088d1f9afdfdce0beea1dda
Saman Farahmand <40132312+samanfrm@users.noreply.github.com>
2019-10-15 10:06:38 -0400

    Update functions.py

--- Scripts/functions.py
+++ Scripts/functions.py
@@ -251,6 +251,26 @@ def get_xml(html, content_tag='ekb', fail_if_empty=False):
     return header + events_terms.replace('\n', '') + footer
 
 
+def dfs(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                stack.append((next, path + [next]))
+
+
 def TRIPS_apiv2(candidate_sentence,query_genes,Positive,Negative):
     for sent in candidate_sentence.itertuples():
         URL='http://trips.ihmc.us/parser/cgi/'
